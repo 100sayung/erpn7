390401349779d3333d945acb6eeb598a4fd5a6fb
tkdgus5512 <70308218+tkdgus5512@users.noreply.github.com>
2020-09-21 19:07:05 +0900

    Revert "1ff"
    This reverts commit 1466168ff9c085746eb6602b3a618cb30b613d92.

---
+++ N7_ERPProject/src/main/java/com/n7/erp/controller/MyInfoController.java
@@ -0,0 +1,79 @@
+package com.n7.erp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.n7.erp.bean.hr.HR_Card;
+import com.n7.erp.service.HrMM;
+import com.n7.erp.service.MemberMM;
+
+
+
+
+@Controller
+public class MyInfoController {
+	
+	ModelAndView mav;
+	
+	@Autowired
+	private MemberMM mm;
+	@Autowired
+	private HrMM hm;
+	//이동관련
+	@GetMapping(value="/myinfo/checkattendance")
+	public String moveCheckAttendance() {
+		return "myInfo/checkAttendance";
+	}
+	@GetMapping(value="/myinfo/applyholiday")
+	public String moveApplyHoliday() {
+		return "myInfo/applyHoliday";
+	}
+	@GetMapping(value="/myinfo/myattendance")
+	public String moveMyAttendance() {
+		return "myInfo/myAttendance";
+	}
+	@GetMapping(value="/myinfo/myholiday")
+	public String moveMyHoliday() {
+		return "myInfo/myHoliday";
+	}
+	
+	//여기까지 이동관련
+	
+	
+	//myinfo 내정보 스스로 등록 메소드
+	@PostMapping(value="/myinfo/newacademic")
+	public String registAcademic(HttpServletRequest request){
+		String id = request.getSession().getAttribute("id").toString();
+		hm.registAcademic(request, id);
+		return "myInfo/myInfo";
+	}
+	//새로운 이력 정보 등록
+	@PostMapping(value="/myinfo/newcareer")
+	public String registCareer(HttpServletRequest request){
+		String id = request.getSession().getAttribute("id").toString();
+		hm.registCareer(request, id);
+		return "myInfo/myInfo";
+	}
+	//새로운 자격증 정보 등록
+	@PostMapping(value="/myinfo/newcertification")
+	public String registCertification(HttpServletRequest request) throws Exception{
+		String id = request.getSession().getAttribute("id").toString();
+		hm.registCertification(request, id);
+		return "myInfo/myInfo";
+	}
+	//새로운 인사카드 등록
+	@PostMapping(value="/myinfo/newhrcard")
+	public String registHRCard(HR_Card hrCard, HttpSession session) {
+		hm.registHRCard(hrCard, session.getAttribute("id").toString(), session.getAttribute("cCode").toString());
+		return "myInfo/myInfo";
+	}
+}
---
+++ N7_ERPProject/src/main/java/com/n7/erp/service/ConsultingBoardMM.java
@@ -0,0 +1,113 @@
+package com.n7.erp.service;
+
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.Gson;
+import com.n7.erp.bean.ConsultingBoard;
+import com.n7.erp.dao.HRIDeptDao;
+import com.n7.erp.dao.IConsultingBoardDao;
+import com.n7.erp.userClass.Paging;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+public class ConsultingBoardMM {
+
+	@Autowired
+	private IConsultingBoardDao CBdao;
+
+	ModelAndView mav = new ModelAndView();
+	//게시판 이동시 목록 출력
+	public ModelAndView moveBoardList() {
+		System.out.println("여기로 들어왔나?");
+		String view=null;
+		mav=new ModelAndView();
+		ArrayList<ConsultingBoard> bList=new ArrayList<ConsultingBoard>();
+		System.out.println("그르게 뭐가 문제일까");
+		bList=CBdao.moveBoardList();
+		System.out.println("게시판 목록 불러오기="+bList);
+		Integer pageNum=bList.size();
+		System.out.println("전체 글 갯수"+pageNum);
+		pageNum=(pageNum==null)?1:pageNum;
+
+		if(pageNum<=0) {
+			System.out.println("잘못된 페이지 번호");
+		}
+		if(bList!=null) {
+			System.out.println("나는 모르겠다");
+			Gson gson=new Gson();
+			String json=gson.toJson(bList);
+			mav.addObject("bList", json);
+			mav.addObject("paging", getPaging(pageNum));
+			view="erpboard";
+		}else {
+			view="home";
+		}
+		mav.setViewName(view);
+		return mav;
+	}
+	//페이징
+	private Object getPaging(Integer pageNum) {
+		int maxNum=CBdao.getBoardList();
+		System.out.println("maxnum="+maxNum);
+		int listCount=5;
+		int pageCount=2;
+		String boardName="erpboard";
+		Paging paging=new Paging(pageNum,maxNum, listCount, pageCount, boardName);
+		System.out.println("======"+paging.makeHtmlPaging());
+		return paging.makeHtmlPaging();
+	}
+	//게시판 글 작성
+	public ModelAndView writeBoard(ConsultingBoard board) {
+		String view=null;
+		if(CBdao.writeBoard(board)) {
+			view="erpapply";
+		}else {
+			view="writeFrm";
+		}
+		mav.setViewName(view);
+		return mav;
+	}
+	//게시글 수정 목록 출력
+	public String boardmodifyajax(Integer num) {
+		System.out.println("수정페이지 서비스 번호 값="+num);
+		ConsultingBoard cbList=CBdao.boardmodifyajax(num);
+		System.out.println("회원 정보="+cbList);
+		if(cbList!=null) {
+			Gson gson=new Gson();
+			String json=gson.toJson(cbList);
+			System.out.println("json타입으로 변경"+json);
+			return json;
+		}
+
+		return null;
+	}
+	//게시글 수정
+	public ModelAndView boardmodify(ConsultingBoard board) {
+		String view=null;
+		if(CBdao.boardmodify(board)) {
+			view="redirect:erpboard";
+		}
+		mav.setViewName(view);
+		return mav;
+	}
+	//게시글 삭제
+	public ModelAndView writelistdelete(Integer num) {
+		String view=null;
+		if(num!=null) {
+			if(CBdao.writelistdelete(num)) {
+				view="erpboard";
+			}else {
+				view="erpboard";
+			}
+		}
+		mav.setViewName(view);
+		return mav;
+	}
+}
